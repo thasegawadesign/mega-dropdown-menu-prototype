48d17651f5114a6307ec076bc1ce3da82ef27402
thasegawadesign <hasegawatatsuya206@gmail.com>
2025-10-28 18:33:08 +0900

    refactor: import path

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
+import DisclosureButton from "@/app/components/DisclosureButton";
 import Logo from "@/app/components/Logo";
 import MegaDropdownMenu from "@/app/components/MegaDropdownMenu";
-import DisclosureButton from "./components/DisclosureButton";
 
 export default function Home() {
   return (
