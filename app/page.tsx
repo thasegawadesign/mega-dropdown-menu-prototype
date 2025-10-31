2396f1f95b633bbd4e83e66277e673883cc08641
thasegawadesign <hasegawatatsuya206@gmail.com>
2025-10-31 21:37:51 +0900

    add: photo

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import DisclosureButton from "@/components/DisclosureButton";
 import Logo from "@/components/Logo";
 import MegaDropdownMenu from "@/components/MegaDropdownMenu";
 import clsx from "clsx";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -15,6 +16,15 @@ export default function Home() {
         <MegaDropdownMenu />
         <DisclosureButton />
       </header>
+      <main>
+        <Image
+          src="/photo.avif"
+          width={5464}
+          height={3640}
+          alt="都市の航空写真"
+          priority
+        />
+      </main>
     </>
   );
 }
