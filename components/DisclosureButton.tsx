a080c0d69fd4551ef68f0917c4db5f3819f372f5
thasegawadesign <hasegawatatsuya206@gmail.com>
2025-10-31 15:10:25 +0900

    style: add styles for upcoming feature section

--- components/DisclosureButton.tsx
+++ components/DisclosureButton.tsx
@@ -6,11 +6,13 @@ export default function DisclosureButton() {
     <>
       <button
         className={
-          "flex cursor-pointer flex-col items-center gap-y-1 px-3.5 py-2"
+          "flex cursor-pointer flex-col items-center justify-center gap-y-1 border-l border-gray-200 px-3.5 py-2 lg:ml-10 lg:h-18 xl:ml-20 2xl:ml-28"
         }
       >
         <Menu className={clsx("h-6 w-6 text-gray-500")} />
-        <span className={"text-[8px] font-medium text-gray-800"}>メニュー</span>
+        <span className={"text-[8px] font-medium text-gray-800 lg:text-[10px]"}>
+          メニュー
+        </span>
       </button>
     </>
   );
--- components/MegaDropdownMenu.tsx
+++ components/MegaDropdownMenu.tsx
@@ -1,11 +1,28 @@
 "use client";
 
 import { MEGA_SECTIONS, MegaSection } from "@/constants/mega-data";
+import clsx from "clsx";
 
 type Props = {
   sections?: MegaSection[];
 };
 
 export default function MegaDropdownMenu({ sections = MEGA_SECTIONS }: Props) {
-  return <></>;
+  return (
+    <>
+      <nav>
+        <ul className={clsx("hidden lg:flex")}>
+          {sections.map((section) => (
+            <li key={section.label}>
+              <button className={clsx("h-18 cursor-pointer px-6")}>
+                <span className={clsx("text-sm font-medium text-gray-800")}>
+                  {section.label}
+                </span>
+              </button>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
+  );
 }
